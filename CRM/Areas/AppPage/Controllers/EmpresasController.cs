c654c00e362059d90bbd0f91137cfffa289767e6
Carlos Pradenas <carlpradenas@gmail.com>
2018-03-28 18:49:40 -0300

    MDN Empresas: Avances en cambios segun reunion con Empresas.

--- CRM/Areas/AppPage/Controllers/EmpresasController.cs
+++ CRM/Areas/AppPage/Controllers/EmpresasController.cs
@@ -15,9 +15,25 @@ namespace CRM.Areas.AppPage.Controllers
         }
 
         // GET: AppPage/Empresas/Nuevo
-        public ActionResult Nuevo()
+        public ActionResult Nuevo(string t)
         {
-            return View();
+            switch(t)
+            {
+                case "F":
+                    return View("Nuevo/Fidelizacion");
+                    
+                case "R":
+                    return View("Nuevo/Retencion");
+                    
+                case "P":
+                    return View("Nuevo/Prospeccion");
+                    
+                default:
+                    return View();
+            }
+
+
+            
         }
     }
 }
