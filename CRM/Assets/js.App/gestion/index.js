4753b1530f1141c34a1ff8411b6948ec968d396c
Carlos Pradenas <carlpradenas@gmail.com>
2018-09-12 16:15:39 -0300

    Avances Contactabilidad

--- CRM/Assets/js.App/gestion/index.js
+++ CRM/Assets/js.App/gestion/index.js
@@ -1601,6 +1601,66 @@ $(function () {
         }
 
 
+        //Contactabilidad
+        var rutAf = trutAfiliado.replace(/\./g, '');
+        rutAf = rutAf.substring(0, rutAf.indexOf('-'));
+
+        $("#bdy_datos_contactos").html("");
+        $.SecGetJSON(BASE_URL + "/motor/api/Contactos/lista-contactos-afi", { RutAfiliado: rutAf }, function (contac) {
+            $.each(contac, function (i, e) {
+                var colorPorc = '';
+                var alertFecha = '';
+                if (e.PorcIndice > 70) {
+                    var colorPorc = 'pull-left badge badge-success'
+                }
+                if (e.PorcIndice > 40 && e.PorcIndice < 69) {
+                    var colorPorc = 'pull-left badge badge-warning'
+                }
+                if (e.PorcIndice < 39) {
+                    var colorPorc = 'pull-left badge badge-danger'
+                }
+                if (e.FechaContacto.toFecha() === "01-01-1900") {
+                    alertFecha = e.FechaContacto.toFecha() + '<i class="badge badge-danger badge-stat badge-icon pull-right add-tooltip" style="position: static; data-toggle="tooltip" data-container="body" data-placement="top" data-original-title="Se debe Actualizar Contacto">!</i>'
+                    $("#afiContac").css({ 'display': 'block' })
+                }
+                else { alertFecha = e.FechaContacto.toFecha() }
+                $("#bdy_datos_contactos")
+                    .append(
+                        $("<tr>")
+                            .append($("<td>").append(
+                                $("<select>").addClass('dropdown-caret').css('width', '88px').css('border-radius', '6px').append(
+                                    $('<option data-icon="fa fa-paint-brush">').val('Seleccione').text("Seleccione..."),
+                                    $('<option>').val(1).text("Valido Presencial"),
+                                    $('<option>').val(2).text("Contacto Valido"),
+                                    $('<option>').val(3).text("Tercero Valido"),
+                                    $('<option>').val(4).text("No Contesta"),
+                                    $('<option>').val(5).text("Buzon de voz"),
+                                    $('<option>').val(6).text("Apagado"),
+                                    $('<option>').val(7).text("Equivocado"),
+                                    $('<option>').val(8).text("No Existe")
+                                ).on('change', function () {
+ 
+                                    var indice = $(this).val();
+                                    var valorD = e.ValorDato;
+                                    var ofici = getCookie("Oficina");
+                                    $.SecGetJSON(BASE_URL + "/motor/api/Contactos/actualiza-indice-contacto", { Indice: indice, RutAfi: rutAf, ValorDato: valorD, Oficina: ofici }, function (datos) {
+                                        $.niftyNoty({
+                                            type: 'success',
+                                            icon: 'pli-like-2 icon-2x',
+                                            message: 'Gestión Guardada correctamente.',
+                                            container: 'floating',
+                                            timer: 5000
+                                        });
+                                    });
+                                })
+                            ))
+                            .append($("<td>").append(e.ValorDato))
+                            .append($("<td>").append(e.TipoDato))
+                            .append($("<td>").append(alertFecha))
+                    );
+            });
+        });
+
     });
 
 
